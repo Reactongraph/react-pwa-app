5a1dee10315fb2be2e1683d8e76cbc936216af04
Reactongraph <js.developer@ongraph.com>
2019-07-05 18:04:02 +0530

    PWA offline feature

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -32,7 +32,8 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      // const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `${process.env.PUBLIC_URL}/sw.js`;
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
@@ -46,7 +47,7 @@ export function register(config) {
               'worker. To learn more, visit https://bit.ly/CRA-PWA'
           );
         });
-      } else {
+      } else {  
         // Is not localhost. Just register service worker
         registerValidSW(swUrl, config);
       }
